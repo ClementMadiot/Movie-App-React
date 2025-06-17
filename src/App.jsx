b7b51921fffa1847658dc21d4765b22009eb0cb8
ClementMadiot <clementmadiot09@gmail.com>
2025-06-17 11:21:18 +0200

    Movie API fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,54 @@
-import { useState } from "react"
-import Search from "./component/Search"
+import { useEffect, useState } from "react";
+import Search from "./component/Search";
 
+const API_BASE_URL = "https://api.themoviedb.org/3";
+const ACCESS_TOKEN = import.meta.env.VITE_API_READ_ACCESS_TOKEN;
+// const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${ACCESS_TOKEN}`,
+  },
+};
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage("");
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      const responce = await fetch(endpoint, API_OPTIONS);
+
+      if (!responce.ok) throw new Error("Failed to fetch movies");
+
+      const data = await responce.json();
+      console.log(data);
+
+      if (data.response === "False") {
+        setErrorMessage(`Error data: ${data.Error}`);
+        setMovieList([]);
+        return;
+      }
+      setMovieList(data.results || []);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+      setErrorMessage("Failed to fetch movies. Please try again later.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
   return (
     <main>
       <div className="pattern" />
@@ -11,14 +56,47 @@ const App = () => {
       <div className="wrapper">
         <header>
           <img src="./hero.png" alt="hero" className="logo" />
-          <h1>Find <span className="text-gradient">Movies</span> You'll Love Without the Hassle</h1>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Love
+            Without the Hassle
+          </h1>
         </header>
 
         <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-
+        <section className="all-movies">
+          <h2 className="text-white">All Movies</h2>
+          {isLoading ? (
+            <p className="text-white">Loading...</p>
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {movieList.map((movie) => (
+                <li key={movie.id} className="movie-card text-white">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                  <h3>{movie.title}</h3>
+                  <div className="flex gap-2">
+                    <img
+                      src="star.svg"
+                      alt="stars"
+                      width="18px"
+                      height="18px"
+                      className="w-auto"
+                    />
+                    <p>{movie.vote_average}</p>
+                  </div>
+                </li>
+              ))}
+            </ul>
+          )}
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
